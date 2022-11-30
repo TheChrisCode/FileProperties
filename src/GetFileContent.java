52642e092c80c1753d6819c090ec88aa94134e76
TheChrisCode <christia.phillips@huttoisd.org>
2022-11-30 10:49:17 -0600

    Christians Update!

--- src/GetFileContent.java
+++ src/GetFileContent.java
@@ -1,2 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class GetFileContent {
+    public static void main(String[] args) {
+        try {
+            File file = new File("C:\\Users\\Christia.Phillips\\Documents\\Java Game\\FileProperties\\testdocument.txt");
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                String fileData = fileReader.nextLine();
+                if (fileData.equals("beatsJames")) {
+                    System.out.println("did beat James");
+                }
+                System.out.println(fileData);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
